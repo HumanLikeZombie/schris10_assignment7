a21973de60ed0531d75eec31269790c3b2ca6389
HumanLikeZombie <spencerchristiansen8@gmail.com>
2025-04-10 22:57:28 -0600

    Update appFile.sh
    Start nginx service.

--- appFile.sh
+++ appFile.sh
@@ -1,1 +1,9 @@
 # This is where the appfile(AppFile.sh) will go.
+# This is the first line of a script called a shebang.
+# It's used in script files to set bash as the default shell for command execution.
+#!/usr/bin/bash
+
+# nginx is a web server often used as a proxy, load balancer, and HTTP cache.
+# This command is used start the nginx service. It will initiate the Nginx processes 
+# for web requests.
+service nginx start
